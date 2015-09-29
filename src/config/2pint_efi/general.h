3a85ce3ebfb8c23fbe7369ea23eac49572216670
Andreas Hammarskjöld <andreas@2pintsoftware.com>
2015-09-29 21:01:40 +0200

    Config + updated DHCP.H with new times for the 2Pint Software 2PXE iPXE loader.

---
+++ src/config/2pint_efi/general.h
@@ -0,0 +1,20 @@
+#define	IMAGE_PNG		/* PNG image support */
+
+#define HTTP_ENC_PEERDIST	/* PeerDist content encoding */
+
+//#define NSLOOKUP_CMD		/* DNS resolving command */
+//#define TIME_CMD		/* Time commands */
+//#define DIGEST_CMD		/* Image crypto digest commands */
+//#define LOTEST_CMD		/* Loopback testing commands */
+//#define VLAN_CMD		/* VLAN commands */
+//#define PXE_CMD		/* PXE commands */
+#define REBOOT_CMD		/* Reboot command */
+//#define POWEROFF_CMD		/* Power off command */
+//#define IMAGE_TRUST_CMD	/* Image trust management commands */
+//#define PCI_CMD		/* PCI commands */
+#define PARAM_CMD		/* Form parameter commands */
+//#define NEIGHBOUR_CMD		/* Neighbour management commands */
+//#define PING_CMD		/* Ping command */
+#define CONSOLE_CMD		/* Console command */
+//#define IPSTAT_CMD		/* IP statistics commands */
+//#define PROFSTAT_CMD		/* Profiling commands */
